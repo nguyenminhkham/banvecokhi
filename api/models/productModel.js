cedc1762792964ceceb40e9bac80806890b1ffe6
unknown <ng.minhkham@gmail.com>
2021-12-28 16:05:20 +0700

    commit

--- api/models/productModel.js
+++ api/models/productModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const ProductSchema = new mongoose.Schema({
     title: {type: String, required: true, unique: true},
     desc: {type: String, required: true},
-    img: { type: String, required: true },
+    img: { type: Array, required: true },
     categories: {type: String, required: true},
     price: {type: Number, required: true},
 },
