b5b28e91b6b26e92ed366779837b4b0f7bb1fdf3
unknown <ng.minhkham@gmail.com>
2022-01-15 15:27:59 +0700

    commit

--- api/controllers/sendMail.js
+++ api/controllers/sendMail.js
@@ -42,12 +42,12 @@ const oauth2Client = new google.auth.OAuth2(
         subject: "Xác nhận email",
         html: `
             <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-            <h2 style="text-align: center; text-transform: uppercase;color: teal;">Chào mừng bạn đến với Tmaster</h2>
+            <h2 style="text-align: center; text-transform: uppercase;color: #01bf71;">Chào mừng bạn đến với Tmaster</h2>
             <p>Xin chúc mừng! Bạn sẽ sẵn sàng tạo tài khoản tại TMASTER sau một vài thao tác.
                 Chỉ cần kích vào nút xác nhận bên dưới.
             </p>
 
-            <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
+            <a href=${url} style="background: #cb2026; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
         
             <p>Nếu nút xác nhận không hoạt động bạn có thể truy cập vào đường link bên dưới</p>
         
--- api/routers/userRouter.js
+++ api/routers/userRouter.js
@@ -8,6 +8,10 @@ import sendMail from '../controllers/sendMail.js'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 dotenv.config()
+import {google} from 'googleapis'
+const {OAuth2} = google.auth
+
+const client = new OAuth2(process.env.MAILING_SERVICE_CLIENT_ID)
 
 const {CLIENT_URL} = process.env
 const userRouter = express.Router()
@@ -34,6 +38,92 @@ userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
     }
     res.status(401).send({message: 'Invalid email or password'})
 }))
+
+userRouter.post('/signin_google', async (req, res) => {
+    try {
+        const {tokenId} = req.body
+
+        const verify = await client.verifyIdToken({idToken: tokenId, audience: process.env.MAILING_SERVICE_CLIENT_ID})
+
+        const {email_verified, email, name, picture} = verify.payload
+
+        const password = email + process.env.GOOGLE_SECRET
+
+        const passwordHash = await bcrypt.hash(password, 12)
+
+        if(!email_verified) return res.status(400).json({msg: "Email varification failed."})
+
+        // if (email_verified) {
+            const user = await User.findOne({email})
+
+            if(user) {
+                const isMatch = await bcrypt.compare(password, user.password)
+                if(!isMatch) return res.status(400).json({msg: "Password is incorrect."})
+
+                    res.send({
+                        _id: user._id,
+                        name: user.name,
+                        email: user.email,
+                        isAdmin: user.isAdmin,
+                        token: generateToken(user),
+                        money: user.money,
+                    })
+
+                
+
+                // const refresh_token = createAccessToken({id: user._id})
+                // res.cookie('refreshtoken', refresh_token, {
+                //     httpOnly: true,
+                //     path: '/api/users/refresh_token',
+                //     maxAge: 7*24*60*60*1000
+                // })
+
+                res.json({msg: "Login success!"})
+            // }
+        } else {
+            const user = new User ({
+                name, email, password: passwordHash, money: 1000000
+            })
+
+            const createdUser = await user.save()
+        res.send({
+            _id: createdUser._id,
+            name: createdUser.name,
+            email: createdUser.email,
+            isAdmin: createdUser.isAdmin,
+            token: generateToken(createdUser),
+            money: createdUser.money,
+        })
+
+            // const refresh_token = createAccessToken({id: user._id})
+            //     res.cookie('refreshtoken', refresh_token, {
+            //         httpOnly: true,
+            //         path: '/api/users/refresh_token',
+            //         maxAge: 7*24*60*60*1000
+            //     })
+                res.json({msg: "Login success!"})
+        }
+
+    } catch (err) {
+        // return res.status(500).json({msg: err.message})
+    }
+})
+
+// userRouter.post('/refresh_token', async (req, res) => {
+//     try {
+//         const rf_token = req.cookies.refreshtoken
+//         if(!rf_token) return res.status(400).json({msg: "Please login now!"})
+
+//         jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+//             if(err) return res.status(400).json({msg: "Please login now!"})
+
+//             const access_token = createAccessToken({id: user.id})
+//             res.json({access_token})
+//         })
+//     } catch (err) {
+//         return res.status(500).json({msg: err.message})
+//     }
+// })
 
 userRouter.post(
     '/register',
